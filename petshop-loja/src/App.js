3866a2cfe1b2cf554960063fb2644b03c7d71c62
LEONARDO IGNACIO FERNANDES <leonardo.ifernandes@senacsp.edu.br>
2025-10-18 12:09:57 -0300

    Consumo de API e criação de componentes e páginas do projeto

--- petshop-loja/src/App.js
+++ petshop-loja/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import './assets/css/base/base.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './paginas/Home';
+import Sobre from './paginas/Sobre';
+import Pagina404 from './paginas/Pagina404';
+import Cabecalho from './components/Cabecalho';
+import Post from './paginas/Post';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Cabecalho />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sobre" element={<Home />} />
+        <Route path='/posts/:id' element={<Post />} />
+        <Route path='*' element={<Pagina404 />} />
+      </Routes>
+    </Router>
   );
 }
-
 export default App;
+
